bcb2611f480dee555699031e2a83d18185dae682
xiongsp <37035570+xiongsp@users.noreply.github.com>
2024-12-01 17:05:19 +0800

    feature: support for cuda 11-

--- docker/centos/build.sh
+++ docker/centos/build.sh
@@ -1,3 +1,11 @@
+# 提取CUDA版本号
+CUDA_VERSION=$(echo ${BASE_IMAGE} | grep -oP 'cuda:\K[0-9]+\.[0-9]+')
+
+# 判断CUDA版本是否小于11
+if [ $(echo "$CUDA_VERSION < 11" | bc) -eq 1 ]; then
+    BASE_IMAGE=$(echo ${BASE_IMAGE} | sed 's|nvidia|vistart|')
+fi
+
 docker build \
     --build-arg BASE_IMAGE=${BASE_IMAGE} \
     --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
